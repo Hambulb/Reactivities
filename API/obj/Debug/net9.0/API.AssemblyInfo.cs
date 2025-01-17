9f3234d0fb4c6dd5febcb45edf8e8377a0b5811c
hchalmers1 <h.chalmers@opusapeiro.com>
2025-01-17 17:01:21 +0000

    WeekEnd 17th Jan

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e9bb7dc19c8b89b4f9d657737bcf764f0308df5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4af292cbe4be11535974c531a4da0e5924c9b6dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
